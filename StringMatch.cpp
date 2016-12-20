6113ca5c944b6a014f9d71d9b361355c0a9d6e12
Siva-Karthi <sivasubramani.612@gmail.com>
2016-12-20 10:30:41 +0530

    Initial commit with gitignore

---
+++ StringMatch.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int str_expr_old()
+{
+    cout << "StringMatch" << endl;
+    char f_name[] = "Siva";
+    char *l_name = "S";
+    char x[10];
+    cout << x;
+    return 0;
+}
